a387d6f45ea76289d9b44d984273d3fce2a3470a
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-03-09 20:55:20 -0700

    commit

--- db/__init__.py
+++ db/__init__.py
@@ -7,7 +7,7 @@ def highRiskUsr(user):
         return True
     except:
         pass
-    if user.username:
+    if user.last_name:
         if (user.last_name in user.first_name) or \
             (user.first_name in user.last_name):
             return True
