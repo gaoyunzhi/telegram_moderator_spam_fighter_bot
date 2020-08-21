5e435bf349648bf322f6bc16d429f895d5785ab2
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-08-20 17:33:12 -0700

    commit

--- setup.py
+++ setup.py
@@ -4,36 +4,21 @@ import sys
 def kill():
 	os.system("ps aux | grep ython | grep moderate | awk '{print $2}' | xargs kill -9")
 
-def setup(mode):
-	if mode == 'kill':
-		kill()
+def setup():
+	kill()
+	if 'kill' in sys.argv:
 		return
 
 	RUN_COMMAND = 'nohup python3 -u moderate.py &'
 
-	if mode != 'debug':
-		r = os.system('pip3 install -r requirements.txt --upgrade')
-		if r != 0:
-			os.system('curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py')
-			os.system('sudo python3 get-pip.py')
-			os.system('rm get-pip.py')
-			os.system('pip3 install -r requirements.txt --upgrade')
-	kill()
-
 	if mode.startswith('debug'):
 		os.system(RUN_COMMAND[6:-2])
 	else:
-		print('run moderator')
 		os.system(RUN_COMMAND)
-		print('start run moderator')
 		if 'notail' not in sys.argv:
-			print('tail moderator')
 			os.system('touch nohup.out')
 			os.system('tail -F nohup.out')
 
 
 if __name__ == '__main__':
-	if len(sys.argv) > 1:
-		setup(sys.argv[1])
-	else:
-		setup('')
+	setup()
