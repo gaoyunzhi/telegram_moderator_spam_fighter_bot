e41885699eb5c572506ea3da3b120e988516f91b
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-08-20 17:53:25 -0700

    commit

--- db.py
+++ db.py
@@ -6,7 +6,8 @@ import plain_db
 default_reason = '非常抱歉，机器人暂时无法判定您的消息，已转交人工审核。我们即将删除您这条发言，请注意保存。'
 
 allowlist = plain_db.loadKeyOnlyDB('allowlist')
-blocklist = plain_db.loadDB('blocklist')
+allowlist = plain_db.loadKeyOnlyDB('kicklist')
+blocklist = plain_db.LargeDB('blocklist')
 
 def highRiskUsr(user):
     name = user.first_name
@@ -22,102 +23,54 @@ def mediumRiskUsr(user):
         return True
     return False
 
-class DB(object):
-    lists = ['KICKLIST', 'WHITELIST']
+def addBlacklist(text):
+    text = text.strip()
+    if not text or len(text) < 2:
+        return 'no action'
+    text = text.lower()
+    blocklist.update(text, 6)
 
-    def readFile(self, filename):
-        with open('db/' + filename) as f:
-            content = [x.strip() for x in f.readlines()]
-            setattr(self, filename, set([x for x in content if x]))
+def badTextScore(text):
+    if matchKey(text, allowlist.items()):
+        return 0, []
+    if not text:
+        return 0, []
+    result = {}
+    for key, score in blocklist.items() + [(item, 10) for item in kicklist.items()]:
+        if key.lower() in text.lower():
+            result[key] = sore
+    return sum(result.values()), result
 
-    def saveFile(self, filename):
-        with open('db/' + filename, 'w') as f:
-            f.write('\n'.join(sorted(getattr(self, filename))))
+def badText(text):
+    score, result = self.badTextScore(text)
+    if score < 10:
+        return
+    return ' '.join(result.keys())
 
-    def __init__(self):
-        for l in self.lists:
-            self.readFile(l)
-        with open('db/BLACKLIST') as f:
-            lines = [x.strip().split(':') for x in f.readlines() if x.strip()]
-        self.BLACKLIST = {line[0].strip().lower(): float(line[1]) 
-            for line in lines if line[0] and line[0].strip()}
+def shouldKick(user):
+    if len(user.first_name or '') + len(user.last_name or '') > 40:
+        return True
+    return self.badText(getDisplayUser(user))
 
-    def saveBlacklist(self):
-        lines = [(k.strip().lower(), v) for (k, v) in self.BLACKLIST.items() 
-            if k and k.strip()]
-        lines = sorted([('%s: %f' % l).rstrip('0').rstrip('.') for l in lines])
-        with open('db/BLACKLIST', 'w') as f:
-            f.write('\n'.join(lines))
-        commit()
+def deleteReasons(msg):
+    if not msg.text:
+        yield (5, None) # shouldn't be here
 
-    def setBadness(self, text, weight):
-        text = text.strip()
-        if not text:
-            return 'no action'
-        text = text.lower()
-        self.BLACKLIST[text] = weight
-        if weight == 0:
-            del self.BLACKLIST[text]
-        self.saveBlacklist()
-        return text + ' badness: ' + str(self.BLACKLIST.get(text, 0))
+    score, result = self.badTextScore(msg.text)
+    if score >= 1: # may need revisit
+        timeout = max(0, 7.5 / (2 ** score - 1) - 2.5) # 拍脑袋
+        yield (timeout, default_reason)
+    if score > 0:
+        yield (60, None)
 
-    def badTextScore(self, text):
-        if matchKey(text, self.WHITELIST):
-            return 0, []
-        if not text:
-            return 0, []
-        result = {}
-        for x in list(self.BLACKLIST.keys()) + list(self.KICKLIST):
-            if x.lower() in text.lower():
-                result[x] = self.BLACKLIST.get(x, 1)
-        return sum(result.values()), result
+    if mediumRiskUsr(msg.from_user):
+        yield (20, '请先设置用户名再发言，麻烦您啦~ 我们将在20分钟后删除您这条发言，请注意保存。')
+    if cnWordCount(msg.text) < 6:
+        yield (60, None)
 
-    def badText(self, text):
-        score, result = self.badTextScore(text)
-        if score < 1:
-            return
-        return ' '.join(result.keys())
+    yield (float('Inf'), None)
 
-    def shouldKick(self, user):
-        if len(user.first_name or '') + len(user.last_name or '') > 80:
-            return True
-        return self.badText(getDisplayUser(user))
-
-    def shouldLog(self, msg):
-        if self.shouldDelete(msg)[0] == float('Inf'):
-            # good msg
-            return False
-        name = getDisplayUser(msg.from_user)
-        if msg.forward_from or msg.forward_date or not msg.text:
-            return False
-        if cnWordCount(msg.text) < 10 or self.badTextScore(msg.text)[0] > 2:
-            return False
-        if self.badText(msg.text):
-            detail = ''
-            if len(msg.text) < 40:
-                detail = ' msg: ' + msg.text
-            return 'text contain: ' + self.badText(msg.text) + detail
-        return False # user name not set
-
-    def shouldDeleteReasons(self, msg):
-        if not msg.text:
-            yield (5, None) # shouldn't be here
-
-        score, result = self.badTextScore(msg.text)
-        if score >= 1: # may need revisit
-            timeout = max(0, 7.5 / (2 ** score - 1) - 2.5) # 拍脑袋
-            yield (timeout, default_reason)
-        if score > 0:
-            yield (60, None)
-
-        if mediumRiskUsr(msg.from_user):
-            yield (20, '请先设置用户名再发言，麻烦您啦~ 我们将在20分钟后删除您这条发言，请注意保存。')
-        if cnWordCount(msg.text) < 6:
-            yield (60, None)
-
-        yield (float('Inf'), None)
-
-    def shouldDelete(self, msg):
+    def shouldDelete(msg):
         name = getDisplayUser(msg.from_user)
         if matchKey(name, self.WHITELIST):
             return float('Inf'), None
@@ -132,20 +85,20 @@ class DB(object):
         if cnWordCount(msg.text) < 2:
             return 0, default_reason
 
-        return sorted(list(self.shouldDeleteReasons(msg)))[0]
+        return sorted(list(self.deleteReasons(msg)))[0]
 
-    def getPermission(self, target):
+    def getPermission(target):
         tid = str(target.id)
         for l in self.lists:
-            if tid in getattr(self, l):
+            if tid in getattr(l):
                 return l[0].lower()
 
-    def record(self, mlist, target):
+    def record(mlist, target):
         tid = str(target.id)
         for l in self.lists:
             if l == mlist:
-                getattr(self, l).add(tid)
+                getattr(l).add(tid)
             else:
-                getattr(self, l).discard(tid)
+                getattr(l).discard(tid)
             self.saveFile(l)
         commit()
